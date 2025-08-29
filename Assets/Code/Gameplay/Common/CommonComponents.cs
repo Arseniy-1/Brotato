ee9ce598cf7aa3f997a17e41179ac1ad25fa5da6
Arseniy-1 <92578166+Arseniy-1@users.noreply.github.com>
2025-08-29 15:40:47 +0300

    AddJenny

--- Assets/Code/Gameplay/Common/CommonComponents.cs
+++
@@ -1,7 +0,0 @@
-﻿using Entitas;
-using UnityEngine;
-
-namespace Code.Gameplay.Common
-{
-     [Game] public class WorldPosition : IComponent { public Vector3 Value; }
-}
---
+++ src/Brotato/Assets/Code/Infrastructure/ECSRunner.cs
@@ -0,0 +1,36 @@
+﻿using Code.Gameplay.Common.Time;
+using UnityEngine;
+using Zenject;
+
+namespace Code.Infrastructure
+{
+    public class ECSRunner : MonoBehaviour
+    {
+        private GameContext _gameContext;
+        private ITimeService _timeService;
+        
+        private BattleFeature _battleFeature;
+
+        private void Construct(GameContext gameContext, ITimeService timeService)
+        {
+            _gameContext = gameContext;
+            _timeService = timeService;
+        }
+
+        private void Start()
+        {
+            _battleFeature = new BattleFeature(_gameContext, _timeService);
+        }
+
+        private void Update()
+        {
+            _battleFeature.Execute();
+            _battleFeature.Cleanup();
+        }
+
+        private void OnDestroy()
+        {
+            _battleFeature.TearDown();
+        }
+    }
+}
